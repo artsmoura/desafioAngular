9b3b1b20dc8ee32b1f46559ff54544a42e98cf21
Moura <arthur.simoes@senado.leg.br>
2023-03-24 15:14:26 -0300

    altera controller e routas dos usuarios no backend

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,8 +5,8 @@ import crypto from 'crypto';
 
 dotenv.config();
 
-export const getUsersUni = (req, res) => {
-    const q = "SELECT u.cod_usuario, u.txt_nome_completo, ig.nomeIgreja FROM tab_dados_usuario as u, inscricao as i, igreja as ig WHERE i.idUsuario = u.cod_usuario and i.idEvento = 33 and u.idIgreja = ig.idIgreja and i.excluido != 1";
+export const getUsers = (req, res) => {
+    const q = "SELECT * FROM usuarios";
 
     db.query(q, (error, data) => {
         if (error) return res.json(error);
@@ -15,53 +15,12 @@ export const getUsersUni = (req, res) => {
     });
 };
 
-export const getUsersMen = (req, res) => {
+export const deleteUser = (req, res) => {
+    const q = "DELETE usuarios FROM usuarios WHERE usuarios.id = ?";
 
-    const q = `SELECT u.cod_usuario, u.txt_nome_completo, ig.nomeIgreja
-    FROM tab_dados_usuario as u, inscricao as i, igreja as ig
-    WHERE i.idUsuario = u.cod_usuario and i.idEvento = 33 
-    and u.idIgreja = ig.idIgreja and i.excluido != 1 and u.bln_sexo = 1`;;
-
-    db.query(q, (error, data) => {
+    db.query(q, [req.params.id], (error, data) => {
         if (error) return res.json(error);
 
-        return res.status(200).json(data);
-    });
-};
-
-export const getUsersWom = (req, res) => {
-    const q = `SELECT u.cod_usuario, u.txt_nome_completo, ig.nomeIgreja 
-                FROM tab_dados_usuario as u, inscricao as i, igreja as ig
-                WHERE i.idUsuario = u.cod_usuario and i.idEvento = 33 and u.idIgreja = ig.idIgreja and i.excluido != 1 and u.bln_sexo = 0`;
-
-    db.query(q, (error, data) => {
-        if (error) return res.json(error);
-
-        return res.status(200).json(data);
-    });
-};
-
-export const login = (req, res) => {
-    const q = "SELECT * FROM tab_usuario WHERE txt_email_usuario = ? OR txt_usuario = ?";
-
-    db.query(q, [req.body.txt_email_usuario, req.body.txt_usuario], (error, data) => {
-        if (error) return res.json(error);
-        if (data.length === 0) return res.status(404).json('Usuario não encontrado');
-
-        const loginPassword = crypto.createHash('md5').update(req.body.txt_senha).digest('hex');
-
-        if (loginPassword !== data[0].txt_senha) return res.status(400).json("Usuario ou senha incorretos");
-
-        const token = jwt.sign({
-            id: data[0].cod_usuario
-        }, process.env.JWTPRIVATEKEY, { expiresIn: "12h" });
-
-        const { txt_senha, ...other } = data[0];
-
-        res
-            .cookie('access_token', token, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 })
-            .status(200)
-            .json(other);
-
+        return res.status(200).json('Usuario Deletado');
     });
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { getUsersMen, getUsersUni, getUsersWom, login } from '../controllers/user.js';
+import { deleteUser, getUsers } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get("/allUsers", getUsersUni);
-router.get("/usersMas", getUsersMen);
-router.get("/usersFem", getUsersWom);
-router.post("/login", login);
+router.get("/", getUsers);
+router.delete("/:id", deleteUser);
 
 export default router;
