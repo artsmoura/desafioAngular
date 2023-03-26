7dcc86d7d315a67525e43fec0000ae46c6257af0
Arthur Moura <mourarthur@outlook.com>
2023-03-26 15:56:09 -0300

    cria modal para ver resultado da votacao

--- client/src/app/views/home/voto-dialog/voto-dialog.component.ts
+++ client/src/app/views/home/voto-dialog/voto-dialog.component.ts
@@ -1,12 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { HttpParams } from '@angular/common/http';
+import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Usuario } from 'src/app/shared/model/usuario.model';
+import { UsuarioService } from 'src/app/shared/service/usuario.service';
+import { VotacaoService } from 'src/app/shared/service/votacao.service';
 
+export interface DialogData {
+  id: number,
+  titulo: string
+}
 @Component({
   selector: 'app-voto-dialog',
   templateUrl: './voto-dialog.component.html',
   styleUrls: ['./voto-dialog.component.css']
 })
-export class VotoDialogComponent implements OnInit {
-  ngOnInit(): void {
+export class VotoDialogComponent implements AfterViewInit {
 
+  displayedColumns: string[] = ['select', 'nome', 'sobrenome', 'genero', 'cidade', 'estado'];
+  dataSource = new MatTableDataSource<Usuario>();
+  selection = new SelectionModel<Usuario>(true, []);
+  title: string
+  usuarioSelect: number
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    public dialogRef: MatDialogRef<VotoDialogComponent>,
+    private rest: VotacaoService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
+
+
+  ngAfterViewInit() {
+    this.getUsuarios()
+    this.dataSource.sort = this.sort;
+  }
+
+  getUsuarios() {
+    this.usuarioService.getUsuarios().subscribe(data => {
+      this.dataSource.data = data
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  radiobuttonClick(row: Usuario) {
+    this.usuarioSelect = row.id
+  }
+
+  computarVoto() {
+
+    let params = {
+      'votacao_id': this.data.id,
+      'usuario_id': this.usuarioSelect
+    }
+
+    this.rest.computarVotacao(params).subscribe()
+    this.onClose()
+  }
+
+  onClose(): void {
+    this.dialogRef.close()
   }
 }
