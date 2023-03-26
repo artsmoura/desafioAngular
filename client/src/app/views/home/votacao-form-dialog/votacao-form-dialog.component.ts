4652b71ed5d87ff3467450db053e8a93a6ee1701
Arthur Moura <mourarthur@outlook.com>
2023-03-26 15:53:45 -0300

    ajusta formulario de criacao e edicao de votacao

--- client/src/app/views/home/votacao-form-dialog/votacao-form-dialog.component.ts
+++ client/src/app/views/home/votacao-form-dialog/votacao-form-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Votacao } from 'src/app/shared/model/votacao.model';
 import { VotacaoService } from 'src/app/shared/service/votacao.service';
 
 @Component({
@@ -11,29 +12,65 @@ import { VotacaoService } from 'src/app/shared/service/votacao.service';
 export class VotacaoFormDialogComponent implements OnInit {
 
   public votacaoForm: FormGroup;
+  votacao: any = {};
+  tipoModal: string
 
   constructor(
     private fb: FormBuilder,
-    private rest: VotacaoService,
-    public dialogRef: MatDialogRef<VotacaoFormDialogComponent>
+    private votacaoService: VotacaoService,
+    public dialogRef: MatDialogRef<VotacaoFormDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Votacao
   ) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+    if (this.data !== null) {
+      this.votacaoService.getVotacao(this.data.id).subscribe(res => {
+        console.log(res);
+        this.votacao = res[0]
+        this.updateForm(this.votacao)
+      })
+      this.tipoModal = 'editar'
+    } else {
+      this.tipoModal = 'criar'
+    }
+
     this.votacaoForm = this.fb.group({
       titulo: ['', [Validators.required]],
       descricao: ['', [Validators.required]]
     });
   }
 
+  updateForm(votacao: Votacao) {
+    this.votacaoForm.patchValue({
+      titulo: votacao.titulo,
+      descricao: votacao.descricao
+    })
+  }
+
   createVotacao() {
-    this.rest.postVotacao(this.votacaoForm.value).subscribe(result => { })
+    this.votacaoService.postVotacao(this.votacaoForm.value).subscribe(result => { })
     this.dialogRef.close()
     this.votacaoForm.reset()
+    this.refresh()
   }
+
+  updateVotacao() {
+    this.votacaoService.updateVotacao(this.votacao.id, this.votacaoForm.value).subscribe()
+    this.dialogRef.close()
+    this.votacaoForm.reset()
+    this.refresh()
+  }
+
+
 
   onClose(): void {
     this.dialogRef.close()
     this.votacaoForm.reset()
+  }
+
+  refresh(): void {
+    window.location.reload();
   }
 
 }
